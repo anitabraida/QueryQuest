9015e1b6dc2997c3d8417a8bdb7485d9515e84c7
Helene Ilvonen <helene.ilvonen@helsinki.fi>
2024-01-24 13:31:05 +0200

    #3 cleaned up code and added print statement

--- text-mining.py
+++ text-mining.py
@@ -6,17 +6,15 @@ list_of_urls = ['https://tyopaikat.oikotie.fi/tyopaikat/helsinki']
 scraped_data = []
 
 for url in list_of_urls:
-    ## Send Request
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         job_titles = soup.find_all(attrs={"data-e2e-component": "job-ad-list-item"})
         
         for title in job_titles:
-            scraped_data.append(title.text.strip())  # Extract text content and strip any leading/trailing whitespaces
-
-        ## Add To Data Output
+            scraped_data.append(title.text.strip())
 
 for title in scraped_data:
     print(title)
 
+print("What are these? These are job titles in Helsinki from oikotie.fi")
