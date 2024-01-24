c866916ca44f99a75a64d235d5718d3419cb0b00
annieriika <anni.moisala@gmail.com>
2024-01-24 13:15:38 +0200

    Issue #3 got job listing titles

--- text-mining.py
+++ text-mining.py
@@ -10,7 +10,7 @@ for url in list_of_urls:
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        job_titles = soup.select('h2')
+        job_titles = soup.find_all(attrs={"data-e2e-component": "job-ad-list-item"})
         
         for title in job_titles:
             scraped_data.append(title.text.strip())  # Extract text content and strip any leading/trailing whitespaces
