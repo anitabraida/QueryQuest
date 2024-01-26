b8ea63c8286f547a195255c006dcce86bd707fcc
Helene Ilvonen <helene.ilvonen@helsinki.fi>
2024-01-26 13:04:11 +0200

    #5 split file into strings of articles

--- Week2.py
+++ Week2.py
@@ -3,3 +3,8 @@ while True:
     if query == "?":
         break
     print(query)
+
+with open('enwiki-20181001-corpus.100-articles.txt', 'r') as file:
+    texts = file.read()
+
+documents = texts.split('</article>')
