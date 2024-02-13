75b2f4aec0509c9a3737a00d9b3f658c1baac9fa
annieriika <anni.moisala@gmail.com>
2024-02-13 11:55:59 +0200

    #11 added job titles from duunitori

--- text_mining.py
+++ text_mining.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-
-def scrape_job_titles(url):
+def scrape_oikotie(url):
+    # Scrapes job titles from oikotie.fi website
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -20,14 +20,28 @@ def scrape_job_titles(url):
         print("Failed to fetch data from URL:", url)
         return []
 
+def scrape_duunitori(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        job_titles = soup.find_all('h3', class_='job-box__title')
 
-list_of_urls = ['https://tyopaikat.oikotie.fi/tyopaikat/helsinki']
+        scraped_data = []
+        for title in job_titles:
+            job_title = title.text.strip().split(",", 1)[0]
+            scraped_data.append(job_title)
 
+        return scraped_data
+    else:
+        print("Failed to fetch data from URL:", url)
+        return []
 
-for url in list_of_urls:
-    job_titles = scrape_job_titles(url)
+def scrape_job_titles():
+    job_titles = []
+   # job_titles.extend(scrape_oikotie('https://tyopaikat.oikotie.fi/tyopaikat/helsinki'))
+    job_titles.extend(scrape_duunitori('https://duunitori.fi/tyopaikat/alue/helsinki'))
+
     for title in job_titles:
-        print(title)
+        print(title)     
 
-
-print("What are these? These are job titles in Helsinki from oikotie.fi")
+scrape_job_titles()
