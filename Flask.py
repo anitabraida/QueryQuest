7e21db9f88cd0af063f9d53c60046408e5a5fd81
annieriika <anni.moisala@gmail.com>
2024-02-13 15:15:52 +0200

    #11 Added the links to the search results

--- Flask.py
+++ Flask.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request
-from text_mining import scrape_job_titles
+from text_mining import scrape_websites
 
 app = Flask(__name__)
 
-# URL to scrape job titles from
-list_of_urls = ['https://tyopaikat.oikotie.fi/tyopaikat/helsinki']
-
 # Scraping job listings
-data = 
+scraped_data = scrape_websites()
 
 
 @app.route('/')
@@ -18,11 +15,13 @@ def index():
 @app.route('/search')
 def search():
     query = request.args.get('query')
+    matches = []
     if query:
         # Filter job titles based on the query
-        matches = [title for title in data if query.lower() in title.lower()]
-    else:
-        matches = []
+        for data in scraped_data:
+            if query.lower() in data['title'].lower():
+                matches.append((data['title'], data['link']))
+
     return render_template('index.html', matches=matches)
 
 
