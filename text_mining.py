5087950249fbd470819804aabb1a0557c7c0db50
annieriika <anni.moisala@gmail.com>
2024-02-16 16:46:59 +0200

    #16 Added job descriptions from scraped websites using the job listing link

--- text_mining.py
+++ text_mining.py
@@ -6,18 +6,24 @@ def scrape_oikotie(url):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         
-        # Scraping job titles
-        job_titles = soup.find_all(attrs={"data-e2e-component": "job-ad-list-item"})
+        # Scraping job titles and links
+        job_ads = soup.find_all('article', class_='job-ad-list-item')
         scraped_data = []
-        for title in job_titles:
-            job_title = title.text.strip().split(",", 1)[0]
-            # Scraping links
-            link_tag = title.find_previous('a')
-            if link_tag and 'href' in link_tag.attrs:
-                link = 'https://tyopaikat.oikotie.fi' + link_tag['href']
-
-                scraped_data.append({'title': job_title, 'link': link})  # Append both title and link
-        
+        for job_ad in job_ads:
+            title_tag = job_ad.find('h2', class_='title')
+            if title_tag:
+                job_title = title_tag.text.strip()
+                link_tag = title_tag.find('a', href=True)
+                if link_tag:
+                    link = 'https://tyopaikat.oikotie.fi' + link_tag['href']
+                    # Get job description
+                    description_response = requests.get(link)
+                    if description_response.status_code == 200:
+                        description_soup = BeautifulSoup(description_response.content, 'html.parser')
+                        paragraphs = description_soup.find_all('p')
+                        job_paragraphs = [p.text.strip() for p in paragraphs]
+                        job_description = '\n'.join(job_paragraphs)    
+            scraped_data.append({'title': job_title, 'link': link, 'description': job_description})    
         return scraped_data
     else:
         print("Failed to fetch the page.")
@@ -37,12 +43,16 @@ def scrape_duunitori(url):
             link_tag = title.find_previous('a')
             if link_tag and 'href' in link_tag.attrs:
                 link = 'https://duunitori.fi' + link_tag['href']
-
-                scraped_data.append({'title': job_title, 'link': link})
-
+                # Get job description
+                description_response = requests.get(link)
+                if description_response.status_code == 200:
+                    description_soup = BeautifulSoup(description_response.content, 'html.parser')
+                    description_div = description_soup.find('div', class_='description-box')
+                    job_description = description_div.find('div', class_='description').text.strip()
+            scraped_data.append({'title': job_title, 'link': link, 'description': job_description})    
         return scraped_data
     else:
-        print("Failed to fetch data from URL:", url)
+        print("Failed to fetch the page.")
         return []
 
 
@@ -50,12 +60,14 @@ def scrape_websites():
     data = []
     data.extend(scrape_oikotie('https://tyopaikat.oikotie.fi/tyopaikat/helsinki'))
     data.extend(scrape_duunitori('https://duunitori.fi/tyopaikat/alue/helsinki'))
+    for job in data[:10]:
+        print("Title:",job['title'])
+        print("Description:", job['description'][:100],"...")
+        print("Link:", job['link'])
+        print()
 
     return data
 
-    #for i in data:
-     #   print("Title:", title['title'])
-      #  print("Link:", title['link'])
-       # print() 
-
 scrape_websites()
+
+# The first line of description is not present on the actual website, do we want to keep it?
