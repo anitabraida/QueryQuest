01b760ff3f96d5db4105188cbdad6eeb9082a31a
anitabraida <braidaanita@gmail.com>
2024-02-23 08:46:15 +0200

    #17 We can now load more than the initial page. Number currently set to 50 pages.

--- text_mining.py
+++ text_mining.py
@@ -2,58 +2,77 @@ from bs4 import BeautifulSoup
 import requests
 
 def scrape_oikotie(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
+    i = 0
+    scraped_data = []
+    while i < 50:
+        if i == 0:
+            URL = url
+        else:
+            URL= url + "?sivu=" + str(i)
+        i += 1
+        response = requests.get(URL)
+    
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            # Scraping job titles
+            job_ads = soup.find_all('article', class_='job-ad-list-item')
         
-        # Scraping job titles and links
-        job_ads = soup.find_all('article', class_='job-ad-list-item')
-        scraped_data = []
-        for job_ad in job_ads:
-            title_tag = job_ad.find('h2', class_='title')
-            if title_tag:
-                job_title = title_tag.text.strip()
-                link_tag = title_tag.find('a', href=True)
-                if link_tag:
-                    link = 'https://tyopaikat.oikotie.fi' + link_tag['href']
-                    # Get job description
-                    description_response = requests.get(link)
-                    if description_response.status_code == 200:
-                        description_soup = BeautifulSoup(description_response.content, 'html.parser')
-                        paragraphs = description_soup.find_all('p')
-                        job_paragraphs = [p.text.strip() for p in paragraphs]
-                        job_description = '\n'.join(job_paragraphs)    
-            scraped_data.append({'title': job_title, 'link': link, 'description': job_description})    
-        return scraped_data
-    else:
-        print("Failed to fetch the page.")
-        return []
-
-
+            for job_ad in job_ads:
+                title_tag = job_ad.find('h2', class_='title')
+                if title_tag:
+                    job_title = title_tag.text.strip()
+                    link_tag = title_tag.find('a', href=True)
+                    if link_tag:
+                        link = 'https://tyopaikat.oikotie.fi' + link_tag['href']
+                        # Get job description
+                        description_response = requests.get(link)
+                        if description_response.status_code == 200:
+                            description_soup = BeautifulSoup(description_response.content, 'html.parser')
+                            paragraphs = description_soup.find_all('p')
+                            job_paragraphs = [p.text.strip() for p in paragraphs]
+                            job_description = '\n'.join(job_paragraphs)    
+                            scraped_data.append({'title': job_title, 'link': link, 'description': job_description})
+        else:
+            print("Failed to fetch data from URL:", url)
+            return []
+    scraped_data = [dict(s) for s in set(frozenset(d.items()) for d in scraped_data)]
+    return scraped_data
+    
+    
 def scrape_duunitori(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # Scraping job titles
-        job_titles = soup.find_all('h3', class_='job-box__title')
-        scraped_data = []
-        for title in job_titles:
-            job_title = title.text.strip().split(",", 1)[0]
-            # Scraping links
-            link_tag = title.find_previous('a')
-            if link_tag and 'href' in link_tag.attrs:
-                link = 'https://duunitori.fi' + link_tag['href']
-                # Get job description
+    i = 0
+    scraped_data = []
+    while i < 50:
+        if i == 0:
+            URL = url
+        else:
+            URL= url + "?sivu=" + str(i)
+        i += 1
+        response = requests.get(URL)
+    
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            # Scraping job titles
+            job_titles = soup.find_all('h3', class_='job-box__title')
+            #scraped_data = []
+            for title in job_titles:
+                job_title = title.text.strip().split(",", 1)[0]
+                # Scraping links
+                link_tag = title.find_previous('a')
+                if link_tag and 'href' in link_tag.attrs:
+                    link = 'https://duunitori.fi' + link_tag['href']
+                    # Get job description
                 description_response = requests.get(link)
                 if description_response.status_code == 200:
                     description_soup = BeautifulSoup(description_response.content, 'html.parser')
                     description_div = description_soup.find('div', class_='description-box')
                     job_description = description_div.find('div', class_='description').text.strip()
-            scraped_data.append({'title': job_title, 'link': link, 'description': job_description})    
-        return scraped_data
-    else:
-        print("Failed to fetch the page.")
-        return []
+                    scraped_data.append({'title': job_title, 'link': link, 'description': job_description})
+        else:
+            print("Failed to fetch data from URL:", url)
+            return []
+    scraped_data = [dict(s) for s in set(frozenset(d.items()) for d in scraped_data)]
+    return scraped_data
 
 
 def scrape_websites():
