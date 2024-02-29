258a732e06f8d471ee413208d15d8a572ac7b2d8
Helene Ilvonen <helene.ilvonen@helsinki.fi>
2024-02-29 21:41:20 +0200

    removed infinite loop

--- boolean_search.py
+++ boolean_search.py
@@ -1,16 +1,23 @@
 import json
 from sklearn.feature_extraction.text import CountVectorizer
 
-#with open('data.json', 'r', encoding='utf-8') as file:
+# with open('data.json', 'r', encoding='utf-8') as file:
 #    scraped_data = json.load(file)
 
-#query = input("Please enter your query here or hit enter to break: ")
-     
+# query = input("Please enter your query here or hit enter to break: ")
+
+
 def boolean_search(scraped_data, query):
-    d = {"and": "&", "AND": "&",
-         "or": "|", "OR": "|",
-         "not": "1 -", "NOT": "1 -",
-         "(": "(", ")": ")"}          # operator replacements
+    d = {
+        "and": "&",
+        "AND": "&",
+        "or": "|",
+        "OR": "|",
+        "not": "1 -",
+        "NOT": "1 -",
+        "(": "(",
+        ")": ")",
+    }  # operator replacements
 
     def rewrite_token(t):
         if t in t2i_title:
@@ -20,17 +27,16 @@ def boolean_search(scraped_data, query):
         else:
             return ""
 
-    def rewrite_query(query): # rewrite every token in the query
+    def rewrite_query(query):  # rewrite every token in the query
         try:
             return " ".join(rewrite_token(t) for t in query.split())
         except TypeError as e:
             print(f"An error occurred: {e}")
             return ""  # or any other default value you prefer
-   
 
-    titles = [data['title'] for data in scraped_data]
-    links = [data['link'] for data in scraped_data]
-    descriptions = [data['description'] for data in scraped_data]
+    titles = [data["title"] for data in scraped_data]
+    links = [data["link"] for data in scraped_data]
+    descriptions = [data["description"] for data in scraped_data]
 
     cv_title = CountVectorizer(lowercase=True, binary=True)
     cv_description = CountVectorizer(lowercase=True, binary=True)
@@ -44,31 +50,32 @@ def boolean_search(scraped_data, query):
     td_matrix_description = dense_matrix_description.T
     td_matrix_title = dense_matrix_title.T
 
-    t2i_title = cv_title.vocabulary_ 
+    t2i_title = cv_title.vocabulary_
     t2i_description = cv_description.vocabulary_
 
     while True:
         query = query
-        if query == "":    
-            break    
+        if query == "":
+            break
 
         try:
             hits_matrix = eval(rewrite_query(query))
         except Exception as e:
             print(f"Unknown term: {query}")
-            continue    
+            break
         hits_list = list(hits_matrix.nonzero()[1])
         matches = []
         for i, doc_idx in enumerate(hits_list):
             match = {
-            'title': titles[doc_idx],
-            'link': links[doc_idx],
-            'description': descriptions[doc_idx]
+                "title": titles[doc_idx],
+                "link": links[doc_idx],
+                "description": descriptions[doc_idx],
             }
             matches.append(match)
-           # print("Matching doc #{:d}: Title: {:s}, Link: {:s}, Description: {:.100s}...".format(i, titles[doc_idx], links[doc_idx], descriptions[doc_idx]))
+            # print("Matching doc #{:d}: Title: {:s}, Link: {:s}, Description: {:.100s}...".format(i, titles[doc_idx], links[doc_idx], descriptions[doc_idx]))
             if i > 5:
                 break
         return matches
 
-#boolean_search(scraped_data, query) 
+
+# boolean_search(scraped_data, query)
