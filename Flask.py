55eebf6b2196013d0ac220e1997e145e871744b4
Helene Ilvonen <helene.ilvonen@helsinki.fi>
2024-03-09 17:22:34 +0200

    added NER to plotting function

--- Flask.py
+++ Flask.py
@@ -7,11 +7,15 @@ import json
 from boolean_search import boolean_search
 import matplotlib.ticker as ticker
 from fuzzy_search import fuzzy_search
+import spacy
 
 # clear the static folder of any remaining plots
 os.system("rm -f static/*.png")
 
 app = Flask(__name__)
+
+nlp_finnish = spacy.load("fi_core_news_sm")
+nlp_english = spacy.load("en_core_web_sm")
 
 file = "data.json"
 scraped_data = []
@@ -29,17 +33,42 @@ with open(file, "r") as f:
 
 mlp.use("Agg")
 
-# create plot that displays trending titles
+# create plot that displays trending titles with NER
 
 
 def generate_trending_plot():
     fig, ax = plt.subplots(figsize=(10, 5))
 
-    # get popular titles
+    custom_stop_words = ["Helsinki"]
+
+    # get popular titles using NER
     job_title_frequencies = {}
-    for title in scraped_data:
-        job_title = title["title"]
-        job_title_frequencies[job_title] = job_title_frequencies.get(job_title, 0) + 1
+    for data in scraped_data:
+        if data["title"]:
+            doc_finnish = nlp_finnish(data["title"])
+            doc_english = nlp_english(data["title"])
+
+            for entity in doc_finnish.ents:
+                if (
+                    entity.label_ not in ["CARDINAL", "GPE", "LOC"]
+                    and not entity.text.isdigit()
+                    and entity.text not in custom_stop_words
+                ):
+                    job_title = entity.text
+                    job_title_frequencies[job_title] = (
+                        job_title_frequencies.get(job_title, 0) + 1
+                    )
+
+            for entity in doc_english.ents:
+                if (
+                    entity.label_ not in ["CARDINAL", "GPE", "LOC"]
+                    and not entity.text.isdigit()
+                    and entity.text not in custom_stop_words
+                ):
+                    job_title = entity.text
+                    job_title_frequencies[job_title] = (
+                        job_title_frequencies.get(job_title, 0) + 1
+                    )
 
     popular_job_titles = sorted(
         job_title_frequencies.items(), key=lambda x: x[1], reverse=True
@@ -47,17 +76,13 @@ def generate_trending_plot():
     popular_titles, frequencies = zip(*popular_job_titles)
     ax.barh(popular_titles, frequencies, color="skyblue", height=0.6)
 
-    # Set labels and rotation for better visualization
     ax.set_xlabel("Frequency", fontsize=14)
-    ax.set_yticklabels(popular_titles, fontsize=14)  # Adjust fontsize as needed
+    ax.set_yticklabels(popular_titles, fontsize=14)
     ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-    plt.xticks(fontsize=14)  # Adjust x-axis tick font size as needed
+    plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     plt.tight_layout(pad=3)
     plt.savefig("static/trending_plot.png")
-
-
-
 
 
 @app.route("/")
@@ -86,19 +111,18 @@ def search_json():
     search_method = request.args.get(
         "method", "tfi_df"
     )  # Default to "tfidf" if no method is provided
-    #matches = []
+    # matches = []
     statement = ""
     if query:
         if search_method == "boolean":
-            matches, statement = boolean_search(query=query, scraped_data=scraped_data, try_switch=False)
+            matches, statement = boolean_search(
+                query=query, scraped_data=scraped_data, try_switch=False
+            )
         elif search_method == "fuzzy":
             matches = fuzzy_search(query=query, scraped_data=scraped_data)
         else:
-            
             matches, statement = get_matches(
-                scraped_data=scraped_data,
-                query = query,
-                try_switch = False
+                scraped_data=scraped_data, query=query, try_switch=False
             )
         matches_as_dict_list = [
             dict(zip(["title", "link", "description"], values)) for values in matches
@@ -106,10 +130,7 @@ def search_json():
     else:
         matches_as_dict_list = scraped_data[-15:]
 
-    response_data = {
-        "matches": matches_as_dict_list,
-        "statement": statement
-    }
+    response_data = {"matches": matches_as_dict_list, "statement": statement}
     return jsonify(response_data)
 
 
